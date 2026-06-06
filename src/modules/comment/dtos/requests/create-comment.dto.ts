17bc7fba1548ffb8fb89caa7a0f1326e4ff4978e
TranPhat-28 <tphat8301@gmail.com>
2026-06-06 17:56:26 +0700

    Add validation for Comment

--- src/modules/comment/dtos/requests/create-comment.dto.ts
+++ src/modules/comment/dtos/requests/create-comment.dto.ts
@@ -1,6 +1,11 @@
 import { AutoMap } from '@automapper/classes';
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class CreateCommentDto {
     @AutoMap()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(1000)
     content!: string;
 }
