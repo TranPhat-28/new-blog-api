a84a6db94d0dac0b5714da16e866959050c1394f
TranPhat-28 <tphat8301@gmail.com>
2026-06-06 17:57:24 +0700

    Add validation for Tag

--- src/modules/tag/dtos/requests/create-tag.dto.ts
+++ src/modules/tag/dtos/requests/create-tag.dto.ts
@@ -1,6 +1,11 @@
 import { AutoMap } from '@automapper/classes';
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class CreateTagDto {
     @AutoMap()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
     name!: string;
 }
