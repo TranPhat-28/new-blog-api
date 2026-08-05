8a0fdecab030cd1ccfeaa2f24a0193a2339b91d2
TranPhat-28 <tphat8301@gmail.com>
2026-08-05 22:34:14 +0700

    Run prettier

--- src/migrations/Migration20260805105659_add_comment_user_relationship.ts
+++ src/migrations/Migration20260805105659_add_comment_user_relationship.ts
@@ -1,16 +1,20 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20260805105659_add_comment_user_relationship extends Migration {
+    override async up(): Promise<void> {
+        this.addSql(
+            `alter table "comment" add column "author_id" uuid not null;`,
+        );
+        this.addSql(
+            `alter table "comment" add constraint "comment_author_id_foreign" foreign key ("author_id") references "user" ("id") on update cascade;`,
+        );
+    }
 
-  override async up(): Promise<void> {
-    this.addSql(`alter table "comment" add column "author_id" uuid not null;`);
-    this.addSql(`alter table "comment" add constraint "comment_author_id_foreign" foreign key ("author_id") references "user" ("id") on update cascade;`);
-  }
+    override async down(): Promise<void> {
+        this.addSql(
+            `alter table "comment" drop constraint "comment_author_id_foreign";`,
+        );
 
-  override async down(): Promise<void> {
-    this.addSql(`alter table "comment" drop constraint "comment_author_id_foreign";`);
-
-    this.addSql(`alter table "comment" drop column "author_id";`);
-  }
-
+        this.addSql(`alter table "comment" drop column "author_id";`);
+    }
 }
